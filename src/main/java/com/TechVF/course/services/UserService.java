e2a6ecc8439ea62ecad0147bd2931ac25225189e
Bruno Santos <brunorjcf@gmail.com>
2020-03-19 09:22:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/TechVF/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.TechVF.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.TechVF.course.entities.User;
+import com.TechVF.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
