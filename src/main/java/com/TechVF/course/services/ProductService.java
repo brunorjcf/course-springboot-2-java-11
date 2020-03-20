c9606164e7b9d20c9125109af3b3de424e8d2267
Bruno Santos <brunorjcf@gmail.com>
2020-03-20 10:00:19 -0300

    Product

---
+++ src/main/java/com/TechVF/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.TechVF.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.TechVF.course.entities.Product;
+import com.TechVF.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
